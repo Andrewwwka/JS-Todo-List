226be7382b360e097889f66de56ed4b74599e2a7
Andrew <andrewalthoff@outlook.com>
2025-10-18 23:20:30 -0500

    initial commit, adding todolist.js and todolist.json files

---
+++ todolist.js
@@ -0,0 +1,23 @@
+let tasks = [{description: "Buy groceries", completed: false, id: 1},
+             {description: "Walk the dog", completed: true, id: 2},
+             {description: "Read a book", completed: false, id: 3}];
+function addTask(description) {
+    const newTask = {
+        description: description,
+        completed: false,
+        id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1
+    };
+    tasks.push(newTask);
+}
+
+
+function getTasks() {
+    return tasks;
+}
+
+module.exports = {
+    addTask,
+    removeTask,
+    toggleTaskCompletion,
+    getTasks
+};  
